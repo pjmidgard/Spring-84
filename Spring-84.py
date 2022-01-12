0ee9ada7ac644a70026a6425ad00b19015ba2dfb
Java Software <pj.midgard@yahoo.com>
2022-01-12 18:23:10 +0000

    Add files via upload

--- Spring-84.py
+++ Spring-84.py
@@ -51,18 +51,12 @@ class compression:
 
                     if namez=="e":
                         i=2
-                    
                  
-
-                    spin=0
-                    c=0
-                    A=0
-                    Spin=0
                     sda4=""
                     sda5=""
                     sda6=""
-                    e4a=""
-                    e4b=""
+                    
+                    
                     ei8=""
                         
                     name = input("What is name of file? ")
@@ -105,40 +99,17 @@ class compression:
                     cvf=2
                     cvf1=0
                     s=""
-                    
-                    e2=0
-                    e3=1
-                    e4=""
-                    ei4=0
-                    ei5=7
-                    
-                    e4=""
-                    
-                    c=2
-                    sw=2
-                    elw=0
-                 
+
                     sda3=""
                     sda2=""
 
                     sda5=""
                     sda6=""
-                    sda7=""
-                    sda8=""
-                    sda9=""
+
                     sda11=""
-                    sda12=""
-                    
-                    sda14=""
-                    sdaB=""
+
                     D=0
 
-                    block=1
-                    block2=0
-                    block3=0
-                    
-                    count=0
-                    
                     x=0
                     x1=0
                     x2=0
@@ -163,18 +134,12 @@ class compression:
                         
                         END_working=0
                         Circle_times2=0
-                        ii=0
-                        sda20=""
-                        sda21=""
-                        sda22=""
+                                   
                         sda23=""
-                        sda24=""
-                        sda25=""
-                        sda26=""
-                        sda27=""
+ 
                         sda18=""
                         sda29=""
-                        Spin=0
+                        
                         SpinS=0
                         while END_working<10:
                        
@@ -230,37 +195,25 @@ class compression:
 
                                     lenf3=len(sda2)
                                 lenf2=len(sda2)
-                                E=0
                                 #print(lenf2)
                                 if i==1:
                                     if lenf7>=(2**40)-1:
                                         raise SystemExit
 
-                                      
-                                      
                                 #########################################################################################################################################################
                                 
-                                block2=0
+                                
                                 if i==1:
 
                                     lenf5=len(sda2)
 
-                                    block2=0
-                                    ei4=0
-                                    ei5=1
-
-                                    
                                     sda3=sda2
-                                    
-                                    block3=0
-                                    Colaider3=""
-                                   
+                                 
                                     lenf5=len(sda3)
                                     
                                     
                                     #Extract
-                                    
-                                    
+                            
                                     s=""
 
                                     sda3=sda2
@@ -269,70 +222,31 @@ class compression:
                                     sda4=""
                                     sda5=""
                                     sda6=""
-                                    sda7=""
-                                    sda8=""
-                                    sda9=""
+                                  
                                     sda17=""
-                                    Bytes_row4=""
-                                    sda19=""
-                                    
-                                    ei=0
-                                   
-	
-
+                   
                                     g=0
 
                                     sda3=sda2
 
-                                    lenf6=len(sda3)
-                        
-                                    count_times4=0
-
-                                    FF=len(sda3)
-                                    
+                                    lenf6=len(sda3)                      
                                     sda17=""
-                                    sda19=""
-                                    
+                                
                                     sda3=sda2
                                     
                                     lenf6=len(sda3)
-                                    ei4=0
-                                    ei5=20
-                                    block3=0
-                                    Colaider3=""
-                                    block2=0
-                                    block3=0
-
+                                
                                     szx=""
 
                                     sda6=""
 
                                     #Compression
 
-                                    sda9=""
-
                                     sda10=""
                                     sda11=""
-                                    sda12=""
-                                    sda13=""
+                                    
                                     sda17=""
-                                    sda17=""
-                                    
-
-                                    ei=0
- 
-                                    lenf6F=lenf6
-                                    Times=lenf6F-16
-
-                                    ei=0
-
-                                   
-                                    ei=0
-                                    T14=0
-                                    T21=0
-
-                                    
-                                    
+                 
                                     if   Circle_times2==0 and SpinS==0:
                                     	sda3="1"+sda3
                                     	SpinS=1
@@ -356,23 +270,17 @@ class compression:
                                                 T7=T7//e
                                                 f=1
                                                 bit="0"
-                                                
-                                                
-                                                        
-                                                
+
                                         else:
                                                 e=e+1
                                                 
                                                 g=g+1
-                                                
-                                                
-                                                
+                                          
                                         if g>(2**Deep)-2:
                                                 bit="1"
                                                 T7=T7-1
                                                 f=1
-                                                
-                        
+                                       
                                     if  g>(2**Deep)-2:
 	                                    e=(2**Deep2)-1
 	                                    g=0
@@ -384,10 +292,7 @@ class compression:
 	                                                T7=T7//e
 	                                                f=1
 	                                                bit="1"
-	                                                
-	                                                
-	                                                        
-	                                                
+	                                           
 	                                        else:
 	                                                e=e+1
 	                                                
@@ -398,10 +303,7 @@ class compression:
 	                                                bit=""
 	                                                
 	                                                f=1       
-                                                
-                                                
-                             
-                                 
+                                               
                                     if ccc==1:
                                     		nameas=name+".bin" 
                                     
@@ -441,21 +343,14 @@ class compression:
                                     lenfS=len(sda17)
                                     #print(lenfS)
 
-                                    
-
-
                                     if ccc==2 and Circle_times2==0:
                                                     sda3=sda3[1:]
                                     
                                    
                                     Circle_times2=Circle_times2+1
-                                    
-                                    
+                          
                                     sda2=sda17
 
-                                    
-                                            
-                                    
                                     if ccc==2:
                                             
                                             sda17=sda3
@@ -469,8 +364,6 @@ class compression:
 
                                     if   lenfS<=Deep3 or ccc==2:
                                     	   
-
-
                                             sda30=bin(Deep)[2:]
                                             lenf=len(sda30)
 
@@ -482,12 +375,9 @@ class compression:
                                                         while z<xc:
                                                          	szx8="0"+szx8
                                                          	z=z+1
-                                
                                                 
                                     if   lenfS<=Deep3 or ccc==2:
                                     	   
-
-
                                             sda29=bin(Circle_times3)[2:]
                                             lenf=len(sda29)
 
@@ -534,11 +424,8 @@ class compression:
                                             lenf=len(sda17)                                           
                                             sda17=szx9+sda31+szx8+sda30+szx7+sda29+szx+sda17
 
-
-
                                     if   lenfS<=Deep3 or ccc==2:
-                                            
-                                           
+                                                
                                     		L=len(sda17)
                                     		n = int(sda17, 2)
                                     		qqwslenf=len(sda17)
@@ -559,58 +446,30 @@ class compression:
                                     		xs=float(x3)
                                     		return print(x3)
                                     		
-                               
-                                        
                                 if i==2:
 
                                     sda17=""
-                                    sda19=""
-                                    
+                              
                                     sda3=sda2
-                                    Spin=0
+                                    
                                     lenf6=len(sda3)
-                                    ei4=0
-                                    ei5=20
-                                    block3=0
-                                    Colaider3=""
-                                    block2=0
-                                    block3=0
 
                                     szx=""
 
                                     sda6=""
 
-                                    #Compression
-
-                                    sda9=""
+                                    #Extract
 
                                     sda10=""
                                     sda11=""
-                                    sda12=""
-                                    sda13=""
+                                  
                                     sda4=""
                                     sda5=""
                                     sda6=""
-                                    sda7=""
-
-                                    ei=0
- 
-                                    lenf6F=lenf6
-
-                                    ei=0
-                                    Spin=0
+                                
                                     T7=0
-                                    T10=0
                                     T9=0
-                                    T12=0
-                                    T22=0
-                                  
-                                    T3=1
-                                    T4=0
-                                    T5=0
-                                    cc=0
-
-                              
+                                 
                                     if C==1:
                                         if   Circle_times2==0:
 
@@ -619,7 +478,6 @@ class compression:
                                                 sda3=sda3[8:]
                                                 lenf6=len(sda3)
 
-                                                
                                                 sda10=sda3[0:16]
                                                 Deep5 = int(sda10, 2)
                                                 Deep5=Deep5+2
@@ -660,16 +518,7 @@ class compression:
                                     sda4=""
                                       
                                     #####################################################################################################################################################
-                                                  
-                                    block2=0
-                                    
-                                    Spinh=0              
-                                    block2=0
-                              
-                                    e4=""
-                                    e4a=""
-                                    e4b=""
-                                    block2=0
+                                   
                                     sda5=""
                                     
                                     
@@ -727,13 +576,9 @@ class compression:
                                             	            	szx="0"+szx
                                             	            	z=z+1
                                             	sda17=szx+sda17
-                                    										
-                                           
-
 
                                             L=len(sda17)
-                                           
-                                          
+                                         
                                             n = int(sda17, 2)
                                             qqwslenf=len(sda17)
                                             qqwslenf=(qqwslenf//8)*2
@@ -754,11 +599,7 @@ class compression:
                                             x3=x2-x
                                             xs=float(x3)
                                             return print(x3)
-                                           
-                                            
-                                           
-                                    
-
+   
 d=compression()
 
 xw1=d.cryptograpy_compression4()
