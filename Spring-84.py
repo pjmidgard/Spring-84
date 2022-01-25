9f4dd54e4e7600992d1d68d0ac3f5e84467f0221
Java Software <pj.midgard@yahoo.com>
2022-01-25 17:25:29 +0000

    Update Spring-84.py

--- Spring-84.py
+++ Spring-84.py
@@ -261,7 +261,7 @@ class compression:
                                     e=(2**Deep2)-1
                                     g=0
                                     f=0
-                                    T6=0
+                                    
                                     while f!=1:
                                         T1=T7%e
                                         if T1==0:
